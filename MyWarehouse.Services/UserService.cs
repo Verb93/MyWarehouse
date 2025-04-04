f712814f4daea8bf67763b65b022bc77a264a149
Verb93 <verbenasiliberti@gmail.com>
2025-04-04 08:49:13 +0200

    Commit singolo per UserService.cs

--- MyWarehouse.Services/UserService.cs
+++ MyWarehouse.Services/UserService.cs
@@ -15,8 +15,6 @@ public class UserService : GenericService<Users, UserDTO>, IUserService
     private readonly IMapper _mapper;
     private readonly IUserRepository _repository;
     private readonly IPasswordService<Users> _passwordService;
-    private readonly ISupplierUserRepository _supplierUserRepository;
-    private readonly IJwtService _jwtService;
     public UserService(
         IUserRepository repository, 
         IMapper mapper, 
@@ -28,82 +26,12 @@ public class UserService : GenericService<Users, UserDTO>, IUserService
         _mapper = mapper;
         _repository = repository;
         _passwordService = passwordService;
-        _supplierUserRepository = supplierUserRepository;
-        _jwtService = jwtService;
     }
 
     public override async Task<IEnumerable<UserDTO>> GetAllAsync()
     {
         var users = await _repository.GetAllWithRoles().ToListAsync();
         return _mapper.Map<IEnumerable<UserDTO>>(users);
-    }
-
-    //registra un nuovo utente
-    //controllando se l'emai è già registrata
-    //hash della password prima di salvarla
-    public async Task<ResponseBase<UserDTO>> RegisterUserAsync(RegisterDTO registerDTO)
-    {
-        var response = new ResponseBase<UserDTO>();
-        try 
-        {
-            bool emailExists = await _repository.GetByEmailAsync(registerDTO.Email) != null;
-            if (string.IsNullOrEmpty(registerDTO.Password))
-            {
-                response = ResponseBase<UserDTO>.Fail("la password non può essere vuota", ErrorCode.ValidationError);
-            }
-            else if (emailExists)
-            {
-                response = ResponseBase<UserDTO>.Fail("utente già esistente", ErrorCode.ValidationError);
-            }
-            else
-            {
-                var user = _mapper.Map<Users>(registerDTO);
-                user.PasswordHash = _passwordService.HashPassword(user, registerDTO.Password);
-                user.CreatedAt = DateTime.UtcNow;
-                user.IsDeleted = false;
-
-                var createdUser = await _repository.AddAsync(user);
-                response = ResponseBase<UserDTO>.Success(_mapper.Map<UserDTO>(createdUser));
-            }
-        }
-        catch (Exception ex)
-        {
-            response = ResponseBase<UserDTO>.Fail($"errore interno: {ex.Message}", ErrorCode.ServiceUnavailable);
-        }
-        return response;
-    }
-
-    //cerca l'utente
-    //controlla la password
-    public async Task<ResponseBase<bool>> AuthenticateUserAsync(LoginDTO loginDTO, HttpContext httpContext)
-    {
-        var response = new ResponseBase<bool>();
-        try
-        {
-            var user = await _repository.GetByEmailAsync(loginDTO.Email);
-
-            if (user == null)
-            {
-                response = ResponseBase<bool>.Fail("Email o password non valide", ErrorCode.ValidationError);
-            }
-            else if (string.IsNullOrEmpty(user.PasswordHash) || !_passwordService.VerifyPassword(user, user.PasswordHash, loginDTO.Password))
-            {
-                response = ResponseBase<bool>.Fail("Email o password non valide", ErrorCode.ValidationError);
-            }
-            else
-            {
-                httpContext.Session.SetString("UserId", user.Id.ToString());
-                httpContext.Session.SetString("UserEmail", user.Email);
-
-                response = ResponseBase<bool>.Success(true);
-            }
-        }
-        catch (Exception ex) 
-        {
-            response = ResponseBase<bool>.Fail($"errore interno: {ex.Message}", ErrorCode.ServiceUnavailable);
-
-        }
-        return response;
     }
 
     //non elimina completamente l'utante, così da non dover cambiare le altre tabelle associate
